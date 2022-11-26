bdc34e5a039ebecc61a1a79515759c7e3b58098f
Hugo Leonardo <huleonardo19@gmail.com>
2022-11-26 19:26:34 -0300

    criado styled component para o main na page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import MainStyled from './styles';
+
 import Apresentation from '../components/Apresentation';
 import Skills from '../components/Skills';
 import About from '../components/About';
 
 export default function Home() {
   return (
-    <main>
+    <MainStyled>
       <Apresentation />
       <Skills />
       <About />
-    </main>
+    </MainStyled>
   );
 }
---
+++ src/pages/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+const MainStyled = styled.main`
+  @media (max-width : 900px) {
+    margin-top: 16%;
+  }
+`
+
+export default MainStyled;

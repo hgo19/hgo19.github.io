f57dd66a8f546e34879c90cd6ab60b09ab4502fa
Hugo Leonardo <huleonardo19@gmail.com>
2022-12-06 11:27:27 -0300

    margin-top main container alterado

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -2,16 +2,21 @@ import styled from "styled-components";
 
 
 const MainStyled = styled.main`
-  margin-top: 5%;
+  margin-top: 8%;
   padding-bottom: 5rem;
   
   @media (max-width : 900px) {
-    margin-top: 5%;
-  }
-
-  @media (max-width : 320px) {
     margin-top: 10%;
   }
+
+  @media (max-width : 500px) {
+    margin-top: 15%;
+  }
+
+  @media (max-width : 350px) {
+    margin-top: 20%;
+  }
+
 `
 
 export default MainStyled;

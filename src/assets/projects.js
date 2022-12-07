5c93490dd7e98d44926efd5930db258108f8e6ae
Hugo Leonardo <huleonardo19@gmail.com>
2022-12-07 16:07:30 -0300

    alguns projetos adicionados a home

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -5,19 +5,19 @@ import trybeWallet from './images/projectsImages/trybeWallet.jpeg';
 
 export const frontEndProjects = [
   {
-    name: 'Projeto Shopping Cart - Trybe',
+    name: 'Projeto Shopping Cart',
     img: shoppingCart,
     techs: 'HTML, CSS e JavaScript',
     repositorio: 'https://github.com/hgo19/shopping-cart',
   },
   {
-    name: 'Projeto Trybetunes - Trybe',
+    name: 'Projeto Trybetunes',
     img: trybeTunes,
     techs: 'HTML, CSS, JavaScript e React',
     repositorio: 'https://github.com/hgo19/trybetunes',
   },
   {
-    name: 'Projeto TrybeWallet - Trybe',
+    name: 'Projeto TrybeWallet',
     img: trybeWallet,
     techs: 'HTML, CSS, JavaScript, React e Redux',
     repositorio: 'https://github.com/hgo19/trybewallet',
@@ -30,4 +30,17 @@ export const backEndProjects = [
     techs: 'NodeJS, MySQL e Docker',
     repositorio: 'https://github.com/hgo19/store-manager',
   },
+];
+
+export const PrincipalProjects = [
+  {
+    name: 'TrybeWallet',
+    techs: 'HTML, CSS, JavaScript, React e Redux',
+    repositorio: 'https://github.com/hgo19/trybewallet',
+  },
+  {
+    name: 'StoreManager',
+    techs: 'NodeJS, MySQL e Docker',
+    repositorio: 'https://github.com/hgo19/store-manager',
+  }
 ];
--- src/components/SomeProjects/index.jsx
+++ src/components/SomeProjects/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { SectionStyled, SectionTitleStyled } from './style';
 
+import { PrincipalProjects } from '../../assets/projects';
+
 export default function SomeProjects() {
   return (
     <SectionStyled>
       <SectionTitleStyled>Principais Projetos</SectionTitleStyled>
+      {PrincipalProjects.map(({name, techs, repositorio}) => (
+        <div key={`${name}-card`}>
+          <h3>{name}</h3>
+          <p>
+            Tecnologias Utilizadas:
+            {' '}
+            {techs}
+          </p>
+          <p>
+            Repositório:
+            {' '}
+            <a href={repositorio} target="_blank" rel="noreferrer">Aqui</a>
+          </p>
+        </div>
+      ))}
     </SectionStyled>
   )
 }

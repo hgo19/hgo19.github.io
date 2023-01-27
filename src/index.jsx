cfa1b4c84758845d59e5bebc7a046c992122fd9e
Hugo <huleonardo19@gmail.com>
2023-01-27 18:23:24 -0300

    retirado browser router do index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import PortifolioProvider from './context/PortifolioProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <PortifolioProvider>
-        <App />
-      </PortifolioProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <PortifolioProvider>
+      <App />
+    </PortifolioProvider>
+  </React.StrictMode>,
 );
